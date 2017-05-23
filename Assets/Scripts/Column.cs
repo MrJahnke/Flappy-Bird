221c2a80d5519ef7915c66892c5c82b18f1f4e6a
Jonathan Jahnke <jdjahnke@gmail.com>
2017-05-23 16:09:05 +0300

    Flappy almost done.

---
+++ Assets/Scripts/Column.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Column : MonoBehaviour {
+
+	private void onTriggerEnter2D (Collider2D other) 
+	{
+		if (other.GetComponent<Bird> () != null) 
+		{
+			GameControl.instance.BirdScored ();
+		}
+	}
+}
+
+/*
+
+using UnityEngine;
+using System.Collections;
+
+public class Column : MonoBehaviour 
+{
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if(other.GetComponent<Bird>() != null)
+		{
+			//If the bird hits the trigger collider in between the columns then
+			//tell the game control that the bird scored.
+			GameControl.instance.BirdScored();
+		}
+	}
+}
+*/
