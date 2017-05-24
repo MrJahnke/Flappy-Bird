2b451a8dc0b75f26e925a8302098a45881be5abe
Jonathan Jahnke <jdjahnke@gmail.com>
2017-05-24 14:54:09 +0300

    Finished

--- Assets/Scripts/Column.cs
+++ Assets/Scripts/Column.cs
@@ -4,30 +4,11 @@ using UnityEngine;
 
 public class Column : MonoBehaviour {
 
-	private void onTriggerEnter2D (Collider2D other) 
+	public void OnTriggerEnter2D (Collider2D other) 
 	{
 		if (other.GetComponent<Bird> () != null) 
 		{
-			GameControl.instance.BirdScored ();
-		}
-	}
-}
-
-/*
-
-using UnityEngine;
-using System.Collections;
-
-public class Column : MonoBehaviour 
-{
-	void OnTriggerEnter2D(Collider2D other)
-	{
-		if(other.GetComponent<Bird>() != null)
-		{
-			//If the bird hits the trigger collider in between the columns then
-			//tell the game control that the bird scored.
 			GameControl.instance.BirdScored();
 		}
 	}
 }
-*/
